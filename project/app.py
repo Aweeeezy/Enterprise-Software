0e31fba2b31e8195a6c15b74e1367d79e0bfb79b
EC2 Default User <ec2-user@ip-172-31-20-150.us-west-2.compute.internal>
2018-04-29 21:40:02 +0000

    reconfigured for deployment

--- project/app.py
+++ project/app.py
@@ -1,10 +1,26 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, session, redirect
+from flask_sso import SSO
 
 app = Flask(__name__)
+sso = SSO(app=app)
 
+SSO_ATTRIBUTE_MAP = {
+    'ADFS_AUTHLEVEL': (False, 'authlevel'),
+    'ADFS_GROUP': (True, 'group'),
+    'ADFS_LOGIN': (True, 'nickname'),
+    'ADFS_ROLE': (False, 'role'),
+    'ADFS_EMAIL': (True, 'email'),
+    'ADFS_IDENTITYCLASS': (False, 'external'),
+    'HTTP_SHIB_AUTHENTICATION_METHOD': (False, 'authmethod'),
+}
+
+app.config['SSO_ATTRIBUTE_MAP'] = SSO_ATTRIBUTE_MAP
 
 @app.route('/')
 def index():
+    if 'user' in session:
+        #return 'Welcome {name}'.format(name=session['user']['nickname'])
+        return render_template('table.html')
     return render_template('index.html')
 
 @app.route('/table')
@@ -19,7 +35,11 @@ def visualizations():
 def ageVisualizations():
     return render_template('age.html')
 
+@app.route('/login')
+def login():
+    session['user'] = True
+    return render_template('table.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=80)
